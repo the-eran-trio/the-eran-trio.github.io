834d618b47e654c38927b7be859765e261bee6b2
Eran <eranks@sbcglobal.net>
2017-12-12 10:10:01 -0500

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+//	 alert('Is this thing on?');
+
+function main() {
+	$('.headerimage').hide();
+	$('.headerimage').fadeIn(3000);
+// Fades in the header
+/*	$('.clickable').hide();
+	$('#futureprojects').on('click', function() {
+		$('.clickable').show();
+	});
+*/
+// Initially hides element, then show's the element when future projects is clicked	
+
+}
+
+$(document).ready(main);
